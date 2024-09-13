63a5348553d783b3e9f13dc825b489227284472c
落雨不悔 <m17762618644@163.com>
2024-09-13 16:29:07 +0800

    chore: 升级uTools版本到v5.2.0

--- src-utools/preload.js
+++ src-utools/preload.js
@@ -1,3 +1,33 @@
-const preload = {};
+const fs = require('fs');
 
-window.preload = preload;
+/**
+ * 获取一个文件
+ * @param options {options: {
+ *     title?: string,
+ *     defaultPath?: string,
+ *     buttonLabel?: string,
+ *     filters?: { name: string, extensions: string[] }[],
+ *     properties?: Array<'openFile' | 'openDirectory' | 'multiSelections' | 'showHiddenFiles' | 'createDirectory' | 'promptToCreate' | 'noResolveAliases' | 'treatPackageAsDirectory' | 'dontAddToRecent'>,
+ *     message?: string,
+ *     securityScopedBookmarks?: boolean
+ *   }} 参数
+ * @return {Promise<Blob>} 返回blob对象
+ */
+function openFile(options) {
+    return new Promise((resolve, reject) => {
+        const path = utools.showOpenDialog(options)
+        if (path) {
+            fs.readFile(path, (err, data) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(new Blob([data], { type: 'application/octet-stream' }));
+            })
+        }
+    })
+}
+
+window.preload = {
+    openFile
+}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,21 @@
 /// <reference types="vite/client" />
+interface OpenFileOption {
+    title ?: string,
+    defaultPath ?: string,
+    buttonLabel ?: string,
+    filters ?: { name: string, extensions: string[] }[],
+    properties ?: Array < 'openFile' | 'openDirectory' | 'multiSelections' | 'showHiddenFiles' | 'createDirectory' | 'promptToCreate' | 'noResolveAliases' | 'treatPackageAsDirectory' | 'dontAddToRecent' >,
+    message ?: string,
+    securityScopedBookmarks ?: boolean
+}
+
 interface Window {
-    preload: {}
+    preload: {
+        /**
+         * 打开一个文件，并返回blob对象
+         * @param options 参数
+         * @return 返回blob对象
+         */
+        openFile(options: OpenFileOption): Promise<Blob>
+    }
 }
